ec9a2b5a0ae562dfc02b97e75476695dfa3eb820
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-20 13:28:25 +0000

    Add Redirect model

--- linksly/models.py
+++ linksly/models.py
@@ -6,3 +6,9 @@ class URL(models.Model):
     long_url = models.TextField()
     date_created = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=200, default='ACTIVE')
+
+
+class Redirect(models.Model):
+    url = models.ForeignKey(to=URL, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    count = models.IntegerField()
