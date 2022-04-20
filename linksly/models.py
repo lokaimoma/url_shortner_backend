49061e7ba376ccbde2e31e2856fe29a40e2e97b6
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-20 13:26:23 +0000

    Create URL model

--- linksly/models.py
+++ linksly/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class URL(models.Model):
+    code = models.CharField(max_length=255, primary_key=True)
+    long_url = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=200, default='ACTIVE')
