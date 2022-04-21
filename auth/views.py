839e17307b5ed4a8a15bfa25fa3e2f63b3a9034c
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-21 17:45:40 +0000

    Add dependencies for factory boy and faker

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def check_username_already_taken(request: Request, *args, **kwargs):
+    try:
+        username = request.data['username']
+        flag = True
+        try:
+            User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            flag = False
+        return Response(data={'taken': flag})
+    except KeyError:
+        return Response(
+            status=status.HTTP_400_BAD_REQUEST,
+            data={
+                'error': 'A query param of key username is expected'
+            })
