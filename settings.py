0666608329bf30537405560f37dc82fef1077cc8
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-23 16:17:53 +0000

    Add token endpoints

--- settings.py
+++ settings.py
@@ -113,6 +113,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+REST_FRAMEWORK = [
+    'rest_framework.authentication.BasicAuthentication',
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from apps.auth_ import views as auth_views
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/check-username-exists/', auth_views.check_username_already_taken, name='check-user-name-exists'),
     path('api/register/', auth_views.RegisterUserView.as_view(), name='register-user'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
