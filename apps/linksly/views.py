031165f6ee96cad80463b5a18f26b4c591fb6066
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-27 15:32:27 +0000

    Add urls route

--- apps/linksly/views.py
+++ apps/linksly/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from apps.linksly.models import URL
+from apps.linksly.serializers import URLSerializer
+
+
+class URLViewSet(viewsets.ModelViewSet):
+    serializer_class = URLSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return URL.objects.filter(user_id=user.id)
--- urls.py
+++ urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from apps.auth_ import views as auth_views
+from apps.linksly import views as linksly_views
+
+router = routers.DefaultRouter()
+router.register(prefix='urls', viewset=linksly_views.URLViewSet, basename='url')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     path('api/check-username-exists/', auth_views.check_username_already_taken, name='check-user-name-exists'),
     path('api/login/', auth_views.login_user, name='login'),
     path('api/register/', auth_views.RegisterUserView.as_view(), name='register-user'),
